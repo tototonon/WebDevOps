d68ee6703b34d030243be99f501d71b735026051
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-03-31 15:01:32 +0200

    Add class Request.php

--- src/Request.php
+++ src/Request.php
@@ -1,10 +1,27 @@
 <?php
-
+declare(strict_types=1);
 
 namespace TononT\Webentwicklung;
 
-
-class Request
+// $headers = apache_request_headers();
+class Request implements IRequest
 {
+    /**
+     * Construct.
+     **/
+    public function __construct()
+    {
+        $this->context    = $_SERVER;
+        $this->parameters = $_REQUEST;
 
-}
+    }//end __construct()
+
+
+    public function getBody()
+    {
+        // TODO: Implement getBody() method.
+
+    }//end getBody()
+
+
+}//end class
