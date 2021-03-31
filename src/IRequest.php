f770825e7e6b2df0de8e562809700d2e54fb1edf
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-03-31 15:01:54 +0200

    Add interface IRequest.php

---
+++ src/IRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace TononT\Webentwicklung;
+
+
+interface IRequest
+{
+    /**
+        Retrieves data from the request body. The Request class must have
+        the implementation for this method
+     **/
+
+
+    public function getBody();
+
+
+}//end interface
