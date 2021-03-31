b793c2df4d5b8b6a495cf2747ec6314e06e901cd
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-04-01 01:23:36 +0200

    Update Request.php
    try to make something useful.

--- src/Request.php
+++ src/Request.php
@@ -1,27 +1,77 @@
 <?php
+
 declare(strict_types=1);
 
 namespace TononT\Webentwicklung;
 
-// $headers = apache_request_headers();
+
 class Request implements IRequest
 {
+
+    private array $context;
+
+    private array $params;
+
+
     /**
      * Construct.
      **/
     public function __construct()
     {
-        $this->context    = $_SERVER;
-        $this->parameters = $_REQUEST;
+        $this->context = getContext();
+        $this->params  = getRequest();
+        $this->ageRequest();
 
     }//end __construct()
 
 
-    public function getBody()
+    public function ageRequest()
     {
-        // TODO: Implement getBody() method.
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Get input field value
+            $age = htmlspecialchars($_REQUEST['fage']);
 
-    }//end getBody()
+            // If input field is empty
+            if (empty($age)) {
+                // Display message "Age is empty"
+                echo 'Age is empty';
+
+                // Else if input field is not empty
+            } else {
+                // Display age
+                echo $age;
+            }
+        }
+
+    }//end ageRequest()
+
+
+    public function setContext(): void
+    {
+        $this->context = $_SERVER['PHP_SELF'];
+
+    }//end setContext()
+
+
+    public function setRequest(): void
+    {
+        $this->context = $_REQUEST;
+
+    }//end setRequest()
+
+
+    public function getContext(): array
+    {
+            return $this->context;
+
+    }//end getContext()
+
+
+    public function getRequest(): array
+    {
+        return $this->params;
+
+    }//end getRequest()
 
 
 }//end class
