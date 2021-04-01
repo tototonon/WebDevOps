120683ba77372f20153a27fdd0ae7ddbafcc1068
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-04-01 20:49:47 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,26 +1,42 @@
 <?php
 
 declare(strict_types=1);
-require __DIR__.'/../vendor/autoload.php';
-//TODO
 
+use TononT\Webentwicklung\Request;
+use TononT\Webentwicklung\Http;
+
+require_once __DIR__ . '/../src/Http.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+
+// TODO
 try {
-    $request = new \TononT\Webentwicklung\Request(); //?
-    $urlPath = "http://tonon.test";
-    if (empty($urlPath)) {
+    $request = new Request();
+    $urlPath = 'http://tonon.test';
+
+    if (empty($urlPath) === true) {
         echo 'path is empty! ';
+        Http::send_http_status(400);
+        exit(1);
     }
 
-    if (isset($request)) {
+    if (isset($request) === true) {
         echo $request;
     } else {
-        if(isset($urlPath)&&$request != null){
-            echo "The path is: ";
+        if ($request !== null) {
+            echo 'The path is: ';
             echo $urlPath;
-            echo "The request is: ";
+            echo 'The request is: ';
             echo $request;
         }
     }
 } catch (Exception $ex) {
-    $ex->getMessage();
+    echo $ex->getMessage();
 }
+
+echo "success";
