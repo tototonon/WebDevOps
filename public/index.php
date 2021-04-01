a20b28ad6138b2ea4e59657226621a55e192aa3a
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-04-01 11:07:57 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,13 +3,17 @@
 declare(strict_types=1);
 
 $urlPath = $_SERVER['REQUEST_URI'];
-if (strpos(strtolower($urlPath), 'hello')) {
-    $personToGreet = 'Stranger';
-    if (isset($_REQUEST['name'])) {
-        $personToGreet = $_REQUEST['name'];
-    }
-    echo 'Hello ' . $personToGreet;
-} else {
+try {
+    if (strpos(strtolower($urlPath), 'hello')) {
+        $personToGreet = 'Stranger';
+        if (isset($_REQUEST['name'])) {
+            $personToGreet = $_REQUEST['name'];
+        }
 
-    echo 'Hello there!';
+        echo 'Hello '.$personToGreet;
+    } else {
+        echo 'Hello there!';
+    }
+} catch (Exception $ex) {
+    $ex->getMessage();
 }
