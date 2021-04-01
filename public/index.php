bbdaf842acb3e0eeae5f5db113d8acbf6485bcb4
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-04-01 13:49:02 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,19 +1,16 @@
 <?php
 
 declare(strict_types=1);
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 try {
     $urlPath = \TononT\Webentwicklung\Request::getUrl();
-    if (strpos(strtolower($urlPath), 'hello')) {
-        $personToGreet = 'Stranger';
-        if (isset($_REQUEST['name'])) {
-            $personToGreet = $_REQUEST['name'];
-        }
+    if (empty($urlPath)) {
+        echo 'path is empty! ';
+    }
 
-        echo 'Hello '.$personToGreet;
-    } else {
-        echo 'Hello there!';
+    if (isset($urlPath)) {
+        echo $urlPath;
     }
 } catch (Exception $ex) {
     $ex->getMessage();
