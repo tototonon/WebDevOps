fc58d15aa206ab9fd3daa5194b56af8487329b59
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-04-07 22:33:09 +0200

    Implement class Response.php

---
+++ src/Response.php
@@ -0,0 +1,74 @@
+<?php
+
+
+namespace TononT\Webentwicklung;
+
+
+use Exception;
+
+require_once __DIR__.'/../src/Http.php';
+
+class Response
+{
+
+    private string $body = "jaa";
+
+    private int $httpstatus = 1;
+
+
+    public function __construct()
+    {
+        $this->body;
+       $this->httpstatus;
+    }//end __construct()
+
+
+    /**
+     * @return integer
+     */
+    public function getHttpstatus(): int
+    {
+        return $this->httpstatus;
+
+    }//end getHttpstatus()
+
+
+    /**
+     * @return string|null
+     */
+    public function getBody(): string
+    {
+        return $this->body;
+
+    }//end getBody()
+
+
+    /**
+     * @param string|null $body
+     */
+    public function setBody(string $body): void
+    {
+        if ($body != null) {
+            $this->body = $body;
+        } else {
+            throw new Exception('Body is null');
+        }
+
+    }//end setBody()
+
+
+    /**
+     * @param integer $httpstatus
+     */
+    public function setHttpstatus(int $httpstatus): void
+    {
+        if($httpstatus != 0 && null) {
+            $this->httpstatus = Http::send_http_status($httpstatus);
+
+        } else {
+            throw new Exception("Http-status-code error");
+        }
+    }//end setHttpstatus()
+
+
+}//end class
