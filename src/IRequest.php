49b88e1e7f6c776ddc075ed2009509f2f994459f
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-04-14 14:44:54 +0200

    Update

--- src/IRequest.php
+++ src/IRequest.php
@@ -10,9 +10,4 @@ interface IRequest
         Retrieves data from the request body. The Request class must have
         the implementation for this method
      **/
-
-
-    public function setUriProperties();
-
-
 }//end interface
