3e4ef12086d6cc0d07532fea3064bc2c823df4e7
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-04-14 14:44:46 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -16,24 +16,16 @@ error_reporting(E_ALL);
 
 try {
     $request = new Request();
-    $urlPath = 'http://tonon.test';
-    if (empty($urlPath) === true) {
-        echo 'path is empty! ';
-        Http::send_http_status(400);
-        exit(1);
-    }
-
     if (isset($request) === true) {
         echo $request;
     } else {
         if ($request !== null) {
-            echo 'The path is: ';
-            echo $urlPath;
             echo 'The request is: ';
             echo $request;
         }
     }
 } catch (Exception $ex) {
     echo $ex->getMessage();
-}
 
+
+}
