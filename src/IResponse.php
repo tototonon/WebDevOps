06d05a6b95c105a0a3aeb40492ed521598687689
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-04-21 09:33:44 +0200

    Implement Interface IResponse.php

---
+++ src/IResponse.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace TononT\Webentwicklung;
+
+
+interface IResponse
+{
+
+    /**
+     * @return string
+     */
+    public function getBody(): string;
+
+    /**
+     * @param string $body
+     */
+    public function setBody(string $body): void;
+
+    /**
+     * @return int
+     */
+    public function getStatusCode(): int;
+
+    /**
+     * @param int $statusCode
+     */
+    public function setStatusCode(int $statusCode): void;
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    public function getHeader(string $name): string;
+
+    /**
+     * @return array
+     */
+    public function getHeaders(): array;
+
+    /**
+     * @param string $name
+     * @param string $header
+     */
+    public function setHeader(string $name, string $header): void;
+
+    /**
+     * @param array $headers
+     */
+    public function setHeaders(array $headers): void;
+
+    /**
+     * @param string $name
+     * @return bool
+     */
+    public function hasHeader(string $name): bool;
+}
