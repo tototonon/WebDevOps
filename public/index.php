121dab0cd671fd21735b44b516b311e64310bb57
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-04-21 14:32:56 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -7,25 +7,21 @@ use TononT\Webentwicklung\Http\Response;
 use TononT\Webentwicklung\Routing;
 use TononT\Webentwicklung\Controller\Blog as BlogController;
 
-
 require __DIR__ . '/../vendor/autoload.php';
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
 
 $request = new Request();
-if (isset($_SERVER['REQUEST_URI'])) {
-    $request->setUrl($_SERVER['REQUEST_URI']);
-}
+$request->setUrl($_SERVER['REQUEST_URI']);
 $request->setParameters($_REQUEST);
+
 
 $response = new Response();
 
 $router = new Routing();
 
-$router->addRoute('/blog/show', [BlogController::class, 'show']);
-$router->route($request, $response);
+    $router->addRoute("/blog/show", [BlogController::class,'show']);
+
+    $router->route($request, $response);
 
 http_response_code($response->getStatusCode());
 echo $response->getBody();
