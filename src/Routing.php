0b348a2f889590e215bbdae40f5e79684963c2a7
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-04-21 14:33:53 +0200

    Update Routing.php class

--- src/Routing.php
+++ src/Routing.php
@@ -7,6 +7,7 @@ namespace TononT\Webentwicklung;
 
 use TononT\Webentwicklung\Http\IRequest;
 use TononT\Webentwicklung\Http\IResponse;
+use function call_user_func;
 
 
 class Routing
@@ -17,17 +18,20 @@ class Routing
      */
     protected array $routes = [];
 
+
     /**
-     * @param string $route
+     * @param string   $route
      * @param callable $controller
      */
     public function addRoute(string $route, callable $controller)
     {
         $this->routes[$route] = $controller;
-    }
+
+    }//end addRoute()
+
 
     /**
-     * @param IRequest $request
+     * @param IRequest  $request
      * @param IResponse $response
      */
     public function route(IRequest $request, IResponse $response)
@@ -35,11 +39,14 @@ class Routing
         $url = strtolower($request->getUrl());
         foreach ($this->routes as $route => $controller) {
             if (strpos($url, $route) !== false) {
-                \call_user_func($controller, $request, $response);
+                call_user_func($controller, $request, $response);
                 return;
             }
         }
 
         $response->setBody('Hello there!');
-    }
-}
+
+    }//end route()
+
+
+}//end class
