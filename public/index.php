82cbd1230802ca3a2f335e4ca71f077083781186
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-04-21 11:35:04 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,17 +6,18 @@ use TononT\Webentwicklung\Request;
 use TononT\Webentwicklung\Http;
 use TononT\Webentwicklung\Response;
 use TononT\Webentwicklung\Routing;
-
+use TononT\Webentwicklung\Controller;
 require_once __DIR__ . '/../src/Http.php';
 require __DIR__ . '/../vendor/autoload.php';
-
 
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 $request = new Request();
-$request->setUrl($_SERVER['REQUEST_URI']);
+if (isset($_SERVER['REQUEST_URI'])) {
+    $request->setUrl($_SERVER['REQUEST_URI']);
+}
 $request->setParameters($_REQUEST);
 
 $response = new Response();
