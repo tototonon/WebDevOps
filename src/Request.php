1d25523a9018d27d6835bd3603d6625f89495e0b
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-04-21 09:33:53 +0200

    Update Request.php

--- src/Request.php
+++ src/Request.php
@@ -6,60 +6,76 @@ namespace TononT\Webentwicklung;
 
 use Exception;
 
-class Request
+class Request implements IRequest
 {
+    /**
+     * @var string
+     */
+    protected $url;
 
-    // contains the request type of the request : GET | POST
-    private static $type = null;
+    /**
+     * @var array
+     */
+    protected $parameters;
 
-    // contains the segments of the request
-    private static $segments = null;
-
-    private static $data = null;
-
-    private static $parameters = null;
-
-    private $httpRequestVar = null;
-
-
-    public function __construct()
+    /**
+     * @return string
+     */
+    public function getUrl(): string
     {
-        $this->setHttpRequestMethod();
-        $this->setInputData();
+        return $this->url;
+    }
 
-    }//end __construct()
-
-
-    // TODO
-    public function hasHeader()
+    /**
+     * @param mixed $url
+     */
+    public function setUrl($url): void
     {
+        $this->url = $url;
+    }
 
-    }//end hasHeader()
-
-
-    public function getHeader()
+    /**
+     * @return array
+     */
+    public function getParameters(): array
     {
+        return $this->parameters;
+    }
 
-    }//end getHeader()
-
-
-    protected function setHttpRequestMethod()
+    /**
+     * @param string $name
+     * @return string
+     */
+    public function getParameter(string $name): string
     {
-        if (!isset($_SERVER['REQUEST_METHOD'])) {
-            return;
-        }
+        return $this->parameters[$name];
+    }
 
-        $this->httpRequestVar = $this->validateHttpRequestMethod($_SERVER['REQUEST_METHOD']);
-
-    }//end setHttpRequestMethod()
-
-
-    public function setUrl(string $url)
+    /**
+     * @param string $name
+     * @param string $parameter
+     */
+    public function setParameter(string $name, string $parameter): void
     {
-        $this->$url = $_SERVER['REQUEST_URI'];
+        $this->parameters[$name] = $parameter;
+    }
 
-    }//end setUrl()
+    /**
+     * @param array $parameters
+     */
+    public function setParameters(array $parameters): void
+    {
+        $this->parameters = $parameters;
+    }
 
+    /**
+     * @param string $name
+     * @return bool
+     */
+    public function hasParameter(string $name): bool
+    {
+        return isset($this->parameters[$name]);
+    }
 
     protected function validateHttpRequestMethod($input)
     {
@@ -73,129 +89,11 @@ class Request
             case 'PUT':
             case 'DELETE':
             case 'HEAD':
-            return $input;
+                return $input;
 
             default:
-            throw new Exception('Unexpected value.');
+                throw new Exception('Unexpected value.');
         }
 
     }//end validateHttpRequestMethod()
-
-
-    // set http Action
-    protected function setInputData()
-    {
-        switch ($this->httpRequestVar) {
-            case 'GET':
-            case 'HEAD':
-                $this->setDataFromGet();
-            break;
-
-            case 'POST':
-                $this->setDataFromPost();
-            break;
-
-            case 'PUT':
-                $this->setDataFromPut();
-            break;
-
-            case 'DELETE':
-                // do nothing, no data to set
-            break;
-
-            default:
-            throw new Exception(
-                "Unmapped httpActionMethod. '{$this->httpRequestVar}'"
-            );
-        }//end switch
-
-    }//end setInputData()
-
-
-    // TODO
-    private function setDataFromGet()
-    {
-
-    }//end setDataFromGet()
-
-
-    private function setDataFromPost()
-    {
-
-    }//end setDataFromPost()
-
-
-    private function setDataFromPut()
-    {
-
-    }//end setDataFromPut()
-
-
-    /*
-     * Returns the request type
-     */
-    public static function getType()
-    {
-        return self::$type;
-
-    }//end getType()
-
-
-    /**
-     * @return null
-     */
-    public static function getUrl()
-    {
-        return self::$url;
-
-    }//end getUrl()
-
-
-    /**
-     * @return null
-     */
-    public static function getSegments()
-    {
-        return self::$segments;
-
-    }//end getSegments()
-
-
-    /**
-     * @return null
-     */
-    public static function getParameters()
-    {
-        return self::$parameters;
-
-    }//end getParameters()
-
-
-    /**
-     * @return null
-     */
-    public static function getData()
-    {
-        return self::$data;
-
-    }//end getData()
-
-
-    /**
-     * @param null $data
-     */
-    public static function setData($data): void
-    {
-        self::$data = $data;
-
-    }//end setData()
-
-
-    public function __toString()
-    {
-        return 'request example ';
-
-    }//end __toString()
-
-
-}//end class
+}
