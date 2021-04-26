e00b68e5f930cec4c2444c5ba6a124e69db1b71e
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-04-26 17:23:21 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,24 +4,30 @@ declare(strict_types=1);
 
 use TononT\Webentwicklung\Http\Request;
 use TononT\Webentwicklung\Http\Response;
-use TononT\Webentwicklung\Routing;
-use TononT\Webentwicklung\Controller\Blog as BlogController;
+use TononT\Webentwicklung\Router;
+use TononT\Webentwicklung\mvc\controller\Blog as BlogController;
+use TononT\Webentwicklung\api\config\Connection;
+use TononT\Webentwicklung\api\config\BlogPosts;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 
 $request = new Request();
+
 $request->setUrl($_SERVER['REQUEST_URI']);
 $request->setParameters($_REQUEST);
-
-
 $response = new Response();
+$router = new Router();
+$router->addRoute("/blog/show", [new BlogController(),"show"]);
+$router->route($request, $response);
+$conn = new Connection();
 
-$router = new Routing();
-
-    $router->addRoute("/blog/show", [BlogController::class,'show']);
-
-    $router->route($request, $response);
-
+$db = $conn->getConnection();
+$blogposts = new BlogPosts($db);
 http_response_code($response->getStatusCode());
 echo $response->getBody();
+echo $_SERVER['PHP_SELF'] . "**";
+
+echo $_SERVER['SERVER_NAME'] . "**";
+
+
