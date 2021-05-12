14c8e245a7c71a82fa84a05fb82fbbed602b7366
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-05-12 22:49:13 +0200

    Implement AddFile.php

---
+++ src/mvc/view/Blog/AddFile.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace TononT\Webentwicklung\mvc\view\Blog;
+
+
+class AddFile extends AbstractShow
+{
+    /**
+     * @return string
+     */
+    protected function getTemplatePath(): string
+    {
+        return '\view\templates\blog\addFile.html';
+    }
+
+}
