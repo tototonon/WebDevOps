96e165a3a056779508009e7756619caa3e79d4fe
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-05-12 22:49:21 +0200

    Update

--- src/Repository/BlogPostsRepository.php
+++ src/Repository/BlogPostsRepository.php
@@ -1,10 +1,11 @@
 <?php
+
 declare(strict_types=1);
 
 namespace TononT\Webentwicklung\Repository;
 
 use TononT\Webentwicklung\mvc\model\BlogPosts;
-use TononT\Webentwicklung\mvc\model\PostFile;
+use TononT\Webentwicklung\mvc\model\FilePost;
 use TononT\Webentwicklung\mvc\view\Blog\Show;
 
 class BlogPostsRepository extends AbstractRepository
@@ -17,45 +18,44 @@ class BlogPostsRepository extends AbstractRepository
         $this->connectToDb();
     }
 
-    public function addFile(): void {
+    /**
+     * @param FilePost $filePost
+     */
+    public function addFile(FilePost $filePost): void
+    {
 
-        $file = $_FILES["file"]["name"];
-        $filetype = $_FILES["file"]["type"];
-        $filesize = $_FILES["file"]["size"];
-        $tempfile = $_FILES["file"]["tmp_name"];
-        $filenameWithDirectory = basename("uploads/" . $file);
-        // Check extension
-        if (move_uploaded_file($tempfile, $filenameWithDirectory)) {
-            echo "<h2>File Uploaded</h2>";
-            echo "<p>You file is uploaded successfully.</p>";
-            echo "<p>File name = <b>$file</b></p>";
-            echo "<p>File type = <b>$filetype</b></p>";
-            echo "<p>File size = <b>$filesize</b></p>";
+            $file = $_FILES["file"]["name"];
+            $filetype = $_FILES["file"]["type"];
+            $filesize = $_FILES["file"]["size"];
+            $tempfile = $_FILES["file"]["tmp_name"];
+            $filenameWithDirectory = basename("uploads/" . $file);
+// Check extension
+            if (move_uploaded_file($tempfile, $filenameWithDirectory)) {
+                echo "<h2>File Uploaded</h2>";
+                echo "<p>You file is uploaded successfully.</p>";
+                echo "<p>File name = <b>$file</b></p>";
+                echo "<p>File type = <b>$filetype</b></p>";
+                echo "<p>File size = <b>$filesize</b></p>";
+                $query = $this->connection->prepare("insert into media(file) value ('.$file'); ");
+                $query->bindParam(':file', $filePost->file);
+                $query->execute();
+            }
 
-        } else {
-            echo "Error occurred during file upload!";
         }
-    }
+
     /**
      * @param BlogPosts $blogPosts
      */
     public function add(BlogPosts $blogPosts): void
     {
 
-        $query = $this->connection->prepare(
-            'insert into blog_posts (title, url_key, author, text, file) values (:title, :urlKey, :author, :text); '
-        );
-
-
-
-            $query->bindParam(':title', $blogPosts->title);
-            $query->bindParam(':urlKey', $blogPosts->urlKey);
-            $query->bindParam(':author', $blogPosts->author);
-            $query->bindParam(':text', $blogPosts->text);
-            $query->bindParam(':file', $blogPosts->file);
-            $query->execute();
-
-        }
+        $query = $this->connection->prepare('insert into blog_posts (title, url_key, author, text) values (:title, :urlKey, :author, :text); ');
+        $query->bindParam(':title', $blogPosts->title);
+        $query->bindParam(':urlKey', $blogPosts->urlKey);
+        $query->bindParam(':author', $blogPosts->author);
+        $query->bindParam(':text', $blogPosts->text);
+        $query->execute();
+    }
 
 
     /**
@@ -69,7 +69,6 @@ class BlogPostsRepository extends AbstractRepository
         $query->execute();
         $query->setFetchMode(\PDO::FETCH_ASSOC);
         $resultData = $query->fetch();
-
         if (!$resultData) {
             return null;
         }
@@ -83,7 +82,4 @@ class BlogPostsRepository extends AbstractRepository
         $result->text = $resultData['text'];
         return $result;
     }
-
-
-
 }
