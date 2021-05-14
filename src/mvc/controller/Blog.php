fb498a4138d6f2891ec4c04f46e50c643b57f387
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-05-14 22:01:35 +0200

    Update Blog.php

--- src/mvc/controller/Blog.php
+++ src/mvc/controller/Blog.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace TononT\Webentwicklung\mvc\controller;
 
+use A\B;
 use TononT\Webentwicklung\Http\IResponse;
 use TononT\Webentwicklung\Http\IRequest;
 use TononT\Webentwicklung\mvc\model\BlogPosts;
@@ -29,20 +30,11 @@ class Blog
      */
     public function add(IRequest $request, IResponse $response): void
     {
-        if($request->hasParameter('file')) {
-            $view = new AddFileView();
-            $response->setBody($view->render([]));
-            Validator::allOf(Validator::notEmpty())->check($request->getParameters()['file']);
-            $filePost = new FilePost();
-            $filePost->file = $request->getParameter('file');
-            $repository = new BlogPostsRepository();
-            $repository->addFile($filePost);
-        }
+
         if (!$request->hasParameter('title')) {
             // render the form
             $view = new AddView();
             $response->setBody($view->render([]));
-
     } else {
             // do the validation here
             Validator::allOf(Validator::notEmpty(), Validator::stringType())->check($request->getParameters()['title']);
@@ -55,7 +47,7 @@ class Blog
                 Validator::stringType()
             )->check($request->getParameters()['author']);
             Validator::allOf(Validator::notEmpty(), Validator::stringType())->check($request->getParameters()['text']);
-
+            //Validator::allOf(Validator::notEmpty(), Validator::stringType())->check($request->getParameters()['file']);
 
             // create a database entry
             $blogPost = new BlogPosts();
@@ -63,6 +55,7 @@ class Blog
             $blogPost->urlKey = $request->getParameter('urlKey');
             $blogPost->author = $request->getParameter('author');
             $blogPost->text = $request->getParameter('text');
+            $blogPost->file = $request->getFile();
             $repository = new BlogPostsRepository();
                 $repository->add($blogPost);
                 $response->setBody('great success');
@@ -89,9 +82,8 @@ class Blog
         // get blog entry from database
             $entry = $repository->getByUrlKey($potentialUrlKey);
         if (!$entry) {
-            header("Location: http://tonon.test");
-            echo "go back to tonon.test";
-            die();
+           throw new NotFoundException();
+
         }
             // escaping the entry fields with htmlspecialchars
             // THIS IS THE BARE MINIMUM HERE! Better go for a serializer oder escaping library
