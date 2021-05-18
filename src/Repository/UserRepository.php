b42e388b3cc3d0228828e11d75588b390e269f22
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-05-18 13:09:41 +0200

    Implement UserRepository.php

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -4,11 +4,38 @@
 namespace TononT\Webentwicklung\Repository;
 
 
+use TononT\Webentwicklung\mvc\model\User;
+
 class UserRepository extends AbstractRepository
 {
-    public function __construct()
+    /**
+     * @param string $username
+     * @return User|null
+     */
+    public function getByUsername(string $username)
     {
-        $this->connectToDb();
+        $query = $this->connection->prepare("select * from users where username=:username");
+        $query->bindParam(':username', $username);
+        $query->execute();
+        $query->setFetchMode(\PDO::FETCH_CLASS, User::class);
+        return $query->fetch();
+    }
+
+    /**
+     * @param User $user
+     * @return User|null
+     */
+    public function update(User $user)
+    {
+        $query = $this->connection->prepare(
+            'update users set username =:username, password = :password where id=:id'
+        );
+        $query->bindParam(':id', $user->id);
+        $query->bindParam(':username', $user->username);
+        $query->bindParam(':password', $user->password);
+        $query->execute();
+        $query->setFetchMode(\PDO::FETCH_CLASS, User::class);
+        return $query->fetch();
     }
 
 
