9c8cd25c829c6a46d99653f7b360faf4189ac338
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-05-18 13:09:05 +0200

    Implement Interface for Session

---
+++ src/mvc/controller/SessionAwareControllerInterface.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace TononT\Webentwicklung\mvc\controller;
+
+
+use TononT\Webentwicklung\Http\Session;
+
+interface SessionAwareControllerInterface
+{
+
+    /**
+     * @return Session
+     */
+    public function getSession(): Session;
+
+}
