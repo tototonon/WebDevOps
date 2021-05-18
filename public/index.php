e87ce0b151427487a2851adacd6c379afafa9a2a
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-05-18 13:08:12 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,11 +4,10 @@ declare(strict_types=1);
 
 use TononT\Webentwicklung\Http\Request;
 use TononT\Webentwicklung\Http\Response;
+use TononT\Webentwicklung\NotFoundException;
 use TononT\Webentwicklung\Router;
 use TononT\Webentwicklung\mvc\controller\Blog as BlogController;
-use TononT\Webentwicklung\mvc\model\Connection;
-use TononT\Webentwicklung\mvc\model\BlogPosts;
-
+use TononT\Webentwicklung\mvc\controller\Auth as AuthController;
 require __DIR__ . '/../vendor/autoload.php';
 
 
@@ -23,10 +22,28 @@ $request->setParameters($_REQUEST);
 
 $response = new Response();
 $router = new Router();
+$router->addRoute('/auth/login', AuthController::class, 'login');
+$router->addRoute('/auth/logout', AuthController::class, 'logout');
+
 $router->addRoute('/blog/show', BlogController::class, 'show');
 $router->addRoute('/blog/add', BlogController::class, 'add');
 
-$router->route($request, $response);
+try {
+    $router->route($request, $response);
+} catch (NotFoundException $exception) {
+    // react on content that cannot be found
+    $response->setStatusCode($exception->getCode());
+    $response->setBody('Sorry, 404');
+} catch (AuthenticationRequiredException $exception) {
+    // react on missing login
+    $response->setStatusCode($exception->getCode());
+    $response->setBody('Ein Login wird benötigt');
+} catch (\Exception $exception) {
+    // react on any exception which we do not catch elsewhere to not expose exception messages
+    $response->setStatusCode(500);
+    $response->setBody('Uh Oh ...');
+}
+
 
 http_response_code($response->getStatusCode());
 echo $response->getBody();
