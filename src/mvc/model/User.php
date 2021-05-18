8643c4c76da179afc2bc85700a244f4efc2ae6e6
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-05-18 13:09:17 +0200

    Implement User.php

---
+++ src/mvc/model/User.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace TononT\Webentwicklung\mvc\model;
+
+
+class User
+{
+    public int $id;
+    public string $username;
+    public string $password;
+
+}
