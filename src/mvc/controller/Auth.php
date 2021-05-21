5440175e9ad76948eb47422969900cecc0fb8df8
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-05-21 20:07:27 +0200

    Update Auth.php

--- src/mvc/controller/Auth.php
+++ src/mvc/controller/Auth.php
@@ -109,5 +109,6 @@ class Auth extends AbstractController
     public function logout(IRequest $request, IResponse $response): void
     {
         $this->getSession()->destroy();
+        $response->setBody("logout");
     }
 }
