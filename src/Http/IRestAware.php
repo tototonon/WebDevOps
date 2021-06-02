4c0dd31b76217cdd8c5295241013b7d5edf2eb36
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-06-02 11:14:26 +0200

    Implement IRestAware.php

---
+++ src/Http/IRestAware.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TononT\Webentwicklung\Http;
+
+
+interface IRestAware
+{
+    /**
+     * @return array
+     */
+    public function getIdentifiers(): array;
+
+    /**
+     * @param array $identifiers
+     */
+    public function setIdentifiers(array $identifiers): void;
+
+}
