ab7aa6e30d3a6fb2ab0bec8cb889722ad173bc4f
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-06-24 14:41:02 +0200

    Implement RssFeed.php
    rss feed to retrieve data from external resource

---
+++ src/mvc/view/RssFeed.php
@@ -0,0 +1,77 @@
+<?php
+
+
+namespace TononT\Webentwicklung\mvc\view;
+
+
+class RssFeed
+{
+
+    var $feed;
+
+    /**
+     * RssFeed constructor.
+     * @param $feed
+     */
+    public function __construct($feed)
+    {
+        $this->feed = $feed;
+    }
+
+
+    function parse()
+
+    {
+        $rss = simplexml_load_file($this->feed);
+
+        $rss_split = array();
+
+
+        foreach ($rss->channel->item as $item) {
+
+            $title = (string) $item->title; // Title
+            $link   = (string) $item->link; // Url Link
+            $description = (string) $item->description; //Description
+
+            $rss_split[] = '
+
+          <div>
+        <a href="'.$link.'" target="_blank" title="" >
+            '.$title.' 
+        </a>
+			<hr>
+          </div>
+';
+        }
+
+        return $rss_split;
+    }
+
+
+
+    function display($numrows,$head)
+    {
+        $rss_split = $this->parse();
+        $i = 0;
+        $rss_data = '
+           <div class="title-head">
+         '.$head.'
+           </div>
+         <div class="feeds-links">';
+
+        while ( $i < $numrows )
+        {
+            $rss_data .= $rss_split[$i];
+            $i++;
+        }
+        $trim = str_replace('', '',$this->feed);
+        $user = str_replace('&lang=en-us&format=rss_200','',$trim);
+
+
+        $rss_data.='</div></div>';
+
+        return $rss_data;
+    }
+
+
+}
