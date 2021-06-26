4eb3061104d9ff43941112a7493b0e98188b63c5
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-06-26 18:54:38 +0200

    Implement Feed.php

---
+++ src/mvc/view/Blog/Feed.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+namespace TononT\Webentwicklung\mvc\view\Blog;
+
+
+class Feed extends AbstractShow
+{
+
+    protected function getTemplatePath(): string
+    {
+        return '\view\templates\home\feed.html';
+    }
+    /**
+     * @param array $data
+     * @return string
+     */
+    public function render(array $data): string
+    {
+        $data['title'] = $data['entry'];
+        $data['description'] = $data['entry'];
+        $data['link'] = $data['entry'];
+        return parent::render($data);
+    }
+    public function show($data): string
+    {
+        return json_encode($data);
+    }
+    public function xmlRender($data)
+    {
+        return simplexml_load_file($data);
+
+    }
+}
