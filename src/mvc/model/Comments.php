5386640794a5ffdb491d2650fe4c5297dfc0ce7a
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-07-04 11:17:06 +0200

    Implement Comments.php
    model

--- src/mvc/model/Comments.php
+++ src/mvc/model/Comments.php
@@ -10,4 +10,30 @@ class Comments
     protected int $id;
     public string $text;
     public string $date;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+
+    /**
+     * @param string $text
+     */
+    public function setText(string $text): void
+    {
+        $this->text = $text;
+    }
 }
