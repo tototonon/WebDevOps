15b7c5c0cf828bfaa5c4b1faef03c663b0a867b8
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-07-05 21:12:44 +0200

    Implement Delete.php & delete.html

---
+++ src/mvc/view/Auth/Delete.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace TononT\Webentwicklung\mvc\view\Auth;
+
+
+use TononT\Webentwicklung\mvc\view\Blog\AbstractShow;
+
+class Delete extends  AbstractShow
+{
+
+    /**
+     * @return string
+     */
+    protected function getTemplatePath(): string
+    {
+        return '/view/templates/auth/login.html';
+    }
+
+    /**
+     * @param array $data
+     * @return string
+     */
+    public function render(array $data): string
+    {
+        $data['title'] = 'Delete';
+        return parent::render($data);
+    }
+}
