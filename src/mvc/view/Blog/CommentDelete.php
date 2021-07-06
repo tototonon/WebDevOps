fa701bf55eba370d40b96d09432446dc2d94b2f0
sWIFtitono <timon.tonon@stud.fh-rosenheim.de>
2021-07-06 12:37:29 +0200

    Implement CommentDelete.php

---
+++ src/mvc/view/Blog/CommentDelete.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TononT\Webentwicklung\mvc\view\Blog;
+
+
+
+class CommentDelete extends AbstractShow
+{
+
+    protected function getTemplatePath(): string
+    {
+        return '\view\templates\blog\delete.html';
+    }
+
+    /**
+     * @param array $data
+     * @return string
+     */
+    public function render(array $data): string
+    {
+        $data['title'] = 'Comments';
+        return parent::render($data);
+    }
+}
